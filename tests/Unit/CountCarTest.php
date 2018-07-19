e92135807ba7b85d47b1a48d5670b97022d06407
diana <dyc3@njit.edu>
2018-07-19 01:24:48 -0400

    Count the number of records

---
+++ tests/Unit/CountCarTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\car;
+
+class CountCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCarCount()
+    {
+        $cars = Car::All();
+        $carsCount = $cars->count();
+        $this->assertEquals(50, $carsCount);
+    }
+}
