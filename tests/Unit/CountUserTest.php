5635a720b8c5c85bb5428865b80d17b1ea9eebbd
diana <dyc3@njit.edu>
2018-07-19 00:36:14 -0400

    count the number of records inserted by the database seed

---
+++ tests/Unit/CountUserTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class CountUserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testCountUser()
+    {
+        $user = User::All();
+        $recordCount = $user->count();
+        $this->assertEquals(51, $recordCount);
+    }
+}
